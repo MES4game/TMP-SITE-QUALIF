e3abe251964719f5bd779f473d24c09fd5e50208
MES4game <mes4.game@gmail.com>
2026-04-09 03:46:48 +0200

    fix 2

--- back/.docker/back/entrypoint.sh
+++ back/.docker/back/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-screen -dmS camisole-vm bash /vm/bootstrap.sh > /mounted/back.log 2>&1
-screen -dmS node-backend pnpm run start > /mounted/back.log 2>&1
-exec "$@" > /mounted/back.log 2>&1
+screen -dmS camisole-vm bash -c '/vm/bootstrap.sh >> back.log 2>&1'
+
+if [ "$#" -eq 0 ]; then
+    pnpm run start >> back.log 2>&1
+else
+    exec "$@"
+fi
--- back/.docker/build.sh
+++ back/.docker/build.sh
@@ -1,3 +1,3 @@
 docker build -t mes4game/site-qualif-back:latest --build-context config=./.docker/back/ -f ./.docker/back/Dockerfile .
 docker build -t mes4game/site-qualif-db:latest -f ./.docker/db/Dockerfile .
-docker compose --env-file ./example.env up -d
+docker compose --file ./.docker/docker-compose.yml --env-file ./.docker/example.env up -d
